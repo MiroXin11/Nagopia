5a6babf86b6082a4e1f7e12b4ac54944863c8dbb
WANG-Ruipeng <783132025@qq.com>
2022-11-12 18:42:37 +0800

    动画机

---
+++ Assets/CharacterArts/Wizard - 01/WizardAnimationControl.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WizardAnimationControl : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetWalkingAnimation(bool isWalking)
+    {
+        animator.SetBool("isWalking", isWalking);
+    }
+
+    public void SetAttackAnimation(bool isAttacking)
+    {
+        animator.SetBool("isAttacking", isAttacking);
+    }
+
+    public void OnAttackPlayEnd()
+    {
+        animator.SetBool("isAttacking", false);
+    }
+}

5cfbd66134a2890ce2a5a2ede7153c842a96acc5
MiroXin11 <3492358075@qq.com>
2022-11-15 23:08:07 +0800

    Changed Animator Controller

--- Assets/CharacterArts/Greek Warrior/WarriorAnimationControl.cs
+++ Assets/CharacterArts/Greek Warrior/WarriorAnimationControl.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class WarriorAnimationControl : MonoBehaviour
 {
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator= GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    //void Update()
+    //{
         
-    }
+    //}
 }

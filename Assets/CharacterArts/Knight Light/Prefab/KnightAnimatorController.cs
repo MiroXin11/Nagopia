38b386c185aa9bb5a6c1e89051a0e21663136e23
MiroXin11 <3492358075@qq.com>
2022-12-12 09:49:51 +0800

    完成版备份

---
+++ Assets/CharacterArts/Knight Light/Prefab/KnightAnimatorController.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Nagopia {
+    public class KnightAnimatorController:CharacterAnimatorController {
+        public override void Attack(Action onCompleteCallback = null) {
+            animator.Play("Attacking");
+            this.AttackEndCallback= onCompleteCallback;
+        }
+    }
+}
